e08582dbf0186d253481a4010cf58f27d8057ffb
francdesse <dessefranc@gmail.com>
2024-03-21 22:00:16 -0400

    Setting up environment task

--- Lesson 1.py
+++ Lesson 1.py
@@ -1,1 +1,12 @@
-print('new test')
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver_path = ChromeDriverManager().install()
+
+service=Service(driver_path)
+driver=webdriver.Chrome()
+driver.maximize_window()
+
+driver.get("https://www.google.com")
+
