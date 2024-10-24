f2c6bbb39e87323080cd5846a6e8afd7a6e2bae9
francdesse <dessefranc@gmail.com>
2024-10-23 22:29:02 -0400

    lesson1

--- Python basics part 1.py
+++ Python basics part 1.py
@@ -1,1 +1,22 @@
-'testing'
+# Lesson 1: variables
+# no special charactors when starting ex: !@#$%^&*() | only _ is allow as the first char
+# the next char can be anything: lower and upper case letters, numbers, and char
+
+
+
+
+
+
+a = 100
+b = 12
+c = a*b
+
+print(c)
+print('hello world!')
+
+if a < 12:
+    c= b+a
+    print(c)
+
+else:
+    print('a is not less 12')
