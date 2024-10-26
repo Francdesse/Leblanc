a40d14e9e1f68628a17f6c01992fe84552a3d7b7
francdesse <dessefranc@gmail.com>
2024-10-25 21:22:24 -0400

    python string

--- Python basics part 1.py
+++ Python basics part 1.py
@@ -2,38 +2,38 @@
 # no special charactors when starting ex: !@#$%^&*() | only _ is allow as the first char
 # the next char can be anything: lower and upper case letters, numbers, and char
 
+#
+# a = 100
+# b = 12
+# c = a*b
+#
+# print(c)
+# print('hello world!')
+#
+# if a < 12:
+#     c= b+a
+#     print(c)
+#
+# else:
+#     print('a is not less 12')
+#
+#
+# #10/24/2024
+# #keywords: are variables which are reserved by the programing language
+# # ex: elif, else, for, if and so on there are 35 in total
+#
+# #finding type
+# a = 100
+# b = 12.0
+# c = 'welcome'
+#
+# print(type(a))
+# print(type(b))
+# print(type(c))
+#
 
-a = 100
-b = 12
-c = a*b
-
-print(c)
-print('hello world!')
-
-if a < 12:
-    c= b+a
-    print(c)
-
-else:
-    print('a is not less 12')
-
-
-#10/24/2024
-#keywords: are variables which are reserved by the programing language
-# ex: elif, else, for, if and so on there are 35 in total
-
-#finding type
-a = 100
-b = 12.0
-c = 'welcome'
-
-print(type(a))
-print(type(b))
-print(type(c))
-
-
-""" 
-Data types:
+"""
+# Data types:
 numeric: int, float, and complex
 text - string ex "test"
 boolean - true and false
@@ -43,11 +43,19 @@ binary - bytes, bytearray, memoryview
 """
 
 
+#10/25/2024
 
+#what is your age? using formating:
 
+Age = 24
+Name = 'Franc'
+Location = 'PA'
 
+print(f'Hello, my name is {Name}, I am {Age}, and I am located in {Location}')
 
-
+print(Name.upper())
+print(Name.lower())
+print(len(Location))
 
 
 
