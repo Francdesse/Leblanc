275156d042bc9c27e46d0b4c1ca374c59227c489
francdesse <dessefranc@gmail.com>
2025-02-04 20:58:59 -0500

    2.4.25

--- udemy Python Classes/Lesson 1.py
+++ udemy Python Classes/Lesson 1.py
@@ -6,14 +6,60 @@ from operator import ifloordiv
 # print(a.lower())
 # print(a.title())
 
-a = 5
-b = 6
-print(a+b)
-print(a-b)
-print(a*b)
-print(a/b)
-print(a//b)
+# a = 5
+# b = 6
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
 
 #2.3.25
 # #learning about bitsor and we dont use them in real life
 #shift operator
+
+#2/4/25
+# floor control
+# if statements,
+# if else statements,
+# if elif statements
+# for loops
+# while loops
+# while loops
+# nested loops
+# break statement
+# continue statement
+# loops with else block
+# pass statement
+
+# if statements,
+# if else statements,
+# if elif statements
+
+# a = int(input('enter a number from a to infinity '))
+# b = int(input('enter second number from a to infinity '))
+# if a > b:
+#     print(f'{a} is greater than {b}')
+# else:
+#     print(f'{b} is greater than {a}')
+
+# For loop
+# syntax: for x in sequence
+#             statement
+
+# x= 'sequence'
+# i=0
+# for b in x:
+#     print(i, b)
+#     i=i+1
+
+# n = int(input('enter a number '))
+# for i in range(1, 11):
+#     print(n, "*" , i , "=" , n*i)
+
+#finding the sum of a list
+list = eval(input('enter a list of numbers'))
+sum = 0
+for i in list:
+    sum = sum + i
+print(sum)
