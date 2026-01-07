ce47223d17406e6f70e0b4bbea6bbd4b35f02b74
2 Trois <2trois@2s-MacBook-Air.local>
2026-01-06 20:59:59 -0500

    lesson 1 & 2 from basics

--- udemy Python Classes/FunctionName.py
+++
@@ -1,8 +0,0 @@
-#functions 2.22.25
-def printing_name():
-    print("My name is Lelalo")
-printing_name()
-
-def printing_name_with_param(name):
-    print(f"My name is {name}")
-printing_name_with_param("Lelalo")
---
+++ udemy Python Classes/Python_lessons/secon5_Basics.py
@@ -0,0 +1,9 @@
+print("Hello, World!")
+
+
+#  variables example:
+# _aS = 9
+# as9 = 9
+
+# DataTypes:
+# int, float, str, bool
