7645a35991ade0a751cf4bde4e22b5a370dd0f22
2 Trois <2trois@2s-MacBook-Air.local>
2026-01-07 20:10:26 -0500

    section 1 from basics complete

--- udemy Python Classes/Python_lessons/secon5_Basics.py
+++ udemy Python Classes/Python_lessons/secon5_Basics.py
@@ -1,4 +1,4 @@
-print("Hello, World!")
+# print("Hello, World!")
 
 
 #  variables example:
@@ -7,3 +7,25 @@ print("Hello, World!")
 
 # DataTypes:
 # int, float, str, bool
+
+'''
+1.7.26
+This is how to do multi-line comments
+'''
+
+# e = "hello"\
+#     "today"\
+#     "is a nice day"
+# print(e)
+#
+# f = e.split("a")
+# print(f)
+
+# a = "yello"
+# b = "cheese"
+# c = a + b
+# print(c)
+
+age = int(input("Enter your age: "))
+location = input("where are you from: ")
+print(f'You are {age} years old and you are from {location}')
