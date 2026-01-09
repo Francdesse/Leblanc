4de4f98c52c1f23372de88ee829714c2182b964d
2 Trois <2trois@2s-MacBook-Air.local>
2026-01-08 20:37:42 -0500

    1.8.26

---
+++ udemy Python Classes/Python_lessons/Control_flow.py
@@ -0,0 +1,28 @@
+# 1.8.26
+# COntrol Flow Mini Project:
+'''
+    time frame: Good Morning - 5 AM to 11 AM
+    time frame: Good Afternoon - 12 PM to 17 PM
+    time frame: Good Evening - 18 PM to 23 PM
+    else night
+'''
+# time = int(input("What time of day is it? "))
+# if time >= 5 and time <= 11:
+#     print("Good Morning")
+# elif time >= 12 and time <= 17:
+#     print("Good Afternoon")
+# elif time >= 18 and time <= 23:
+#     print("Good Evening")
+# elif time <0 or time > 23:
+#     print("invalid time, try again")
+# else:
+#     print("Good Night")
+#
+
+'''
+-----------------------------------------------------------
+For loops
+'''
+a = 12
+for i in range(a+2):
+    print(i, a)
