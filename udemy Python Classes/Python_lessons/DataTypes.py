8d050d9bc1b0ea823b3b5f48f2d15ce499e8b68a
2 Trois <2trois@2s-MacBook-Air.local>
2026-01-11 18:36:23 -0500

    1.11.26

---
+++ udemy Python Classes/Python_lessons/DataTypes.py
@@ -0,0 +1,21 @@
+'''
+-----------------------------------------------------------
+1.11.26
+'''
+# list
+
+emp = ['Mark', 33, 'Manager']
+comp = ['Apple', '123 ave', 'NY']
+comp[2] = 'CA' #upding the list
+
+print(f'my name is {emp[0]}, I am {emp[1]} years old working as a {emp[2]}\
+ I currently work at {comp[0]} in {comp[2]} located at {comp[1]}')
+
+# to append is to add something new to the list
+comp.append('HR')
+print(f'my name is {emp[0]}, I am {emp[1]} years old working as a {emp[2]}\
+ I currently work at {comp[0]} in {comp[2]} located at {comp[1]}.\
+ I would glady refer you to my {comp[3]} department')
+
+comp.remove('HR') #to remove something from the list/
+print(comp)
