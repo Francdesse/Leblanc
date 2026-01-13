55ff5dea1359913b0fb0eb52a1d6d7b2ec1d7cbd
2 Trois <2trois@2s-MacBook-Air.local>
2026-01-12 20:45:09 -0500

    1.12.26

--- udemy Python Classes/Python_lessons/DataTypes.py
+++ udemy Python Classes/Python_lessons/DataTypes.py
@@ -3,19 +3,30 @@
 1.11.26
 '''
 # list
+#
+# emp = ['Mark', 33, 'Manager']
+# comp = ['Apple', '123 ave', 'NY']
+# comp[2] = 'CA' #upding the list
+#
+# print(f'my name is {emp[0]}, I am {emp[1]} years old working as a {emp[2]}\
+#  I currently work at {comp[0]} in {comp[2]} located at {comp[1]}')
+#
+# # to append is to add something new to the list
+# comp.append('HR')
+# print(f'my name is {emp[0]}, I am {emp[1]} years old working as a {emp[2]}\
+#  I currently work at {comp[0]} in {comp[2]} located at {comp[1]}.\
+#  I would glady refer you to my {comp[3]} department')
+#
+# comp.remove('HR') #to remove something from the list/
+# print(comp)
 
-emp = ['Mark', 33, 'Manager']
-comp = ['Apple', '123 ave', 'NY']
-comp[2] = 'CA' #upding the list
+# Dictionary
 
-print(f'my name is {emp[0]}, I am {emp[1]} years old working as a {emp[2]}\
- I currently work at {comp[0]} in {comp[2]} located at {comp[1]}')
-
-# to append is to add something new to the list
-comp.append('HR')
-print(f'my name is {emp[0]}, I am {emp[1]} years old working as a {emp[2]}\
- I currently work at {comp[0]} in {comp[2]} located at {comp[1]}.\
- I would glady refer you to my {comp[3]} department')
-
-comp.remove('HR') #to remove something from the list/
-print(comp)
+client = {
+            "name": input("Enter your name: "),
+            "age": int(input("Enter your age: ")),
+            "job": input("Enter your job title: ")
+}
+print(client)
+del client["age"]
+print(client)
