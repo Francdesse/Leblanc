43013d934a3b822c68402d290a5c7ed381e3d1e2
2 Trois <2trois@2s-MacBook-Air.local>
2026-01-20 19:49:31 -0500

    1.20.26

--- udemy Python Classes/selenium_examples/seleium_Basics.py
+++ udemy Python Classes/selenium_examples/seleium_Basics.py
@@ -1,5 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC #allows you to use different wait locators
 from time import sleep
+from selenium.webdriver.common.by import By #allows you to use different locators by seperating them
+from selenium.webdriver.common.keys import Keys #allows you to use keyboard shortcuts
+from selenium.webdriver.support.wait import WebDriverWait
 
 driver = webdriver.Safari()
 
@@ -17,7 +21,12 @@ print(title)
 
 sleep(2)
 
-driver.find_element(by='xpath', value='//*[@id="APjFqb"]').send_keys('Selenium')
+search_box = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="APjFqb"]')))
+search_box.send_keys('Selenium' + Keys.ENTER)
+
+sleep(2)
+# driver.find_element(by='xpath', value='//*[@id="APjFqb"]').send_keys('Selenium')
+
 
 
 driver.quit()
