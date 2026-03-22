f4a087f1f1aff0fb141dc8ef1d3a7cf327e4a07b
2 Trois <2trois@2s-MacBook-Air.local>
2026-03-22 18:29:14 -0400

    Api Testing/partsing json 3.22.26

--- JsonParsing.py
+++ JsonParsing.py
@@ -11,14 +11,21 @@ empl_data = json.loads(importData_toString)
 # print(type(empl_data))
 # print(empl_data)
 
+emp = empl_data['employees']['employee']
 emp1 = empl_data['employees']['employee'][0]
 emp2 = empl_data['employees']['employee'][1]
 emp3 = empl_data['employees']['employee'][2]
 
 
-print(emp1['id'])
-print(emp1['firstName'])
-print(emp1['lastName'])
-print(emp1['photo'])
-# print(emp2)
-# print(emp3)
+# print(emp1['id'])
+# print(emp1['firstName'])
+# print(emp1['lastName'])
+# print(emp1['photo'])
+
+
+#validating that it finds the correct employee
+for empl in emp:
+
+    if empl['firstName'] == 'Robert':
+        assert empl['id'] == '3', f'Employee id is not the same as {empl['id']}'
+
