96f0db054ee70a04b13a0f8fe0e34268f2d856b9
2 Trois <2trois@2s-MacBook-Air.local>
2026-09-21 18:58:26 -0400

    Python Basics Day1

--- Learning_Python_Basics/Day1.py
+++ Learning_Python_Basics/Day1.py
@@ -1,15 +1,52 @@
-a,b,c = 1,2,"test"
-print(f'were testing something {a}, {b}, and {c}')
+# a,b,c = 1,2,"test"
+# print(f'were testing something {a}, {b}, and {c}')
+#
+# #List: uses []
+# test = [1,2,"test", 9]
+#
+# print(test) #printing the whole list
+# test.insert(3, "new") #another way of adding a value to the list, but you can pick where you want to add it
+# print(test)
+#
+# test.append("Bird") #add item at the end of the list
+# print(test)
+#
+# del test[0] # remove item from the list
+# print(test)
 
-#List: uses []
-test = [1,2,"test", 9]
+#Tuple: uses () - are immutable which means you cannot change them
 
-print(test) #printing the whole list
-test.insert(3, "new") #another way of adding a value to the list, but you can pick where you want to add it
-print(test)
+#Dictionary: uses {} - are mutable which means you can change them
 
-test.append("Bird") #add item at the end of the list
-print(test)
+# stu = {"First name": "John", "Last name": "Doe", "Grade": 9, "Class": "Algebra"}
+# # print(stu)
+# # print(stu["First name"],stu["Grade"])
 
-del test[0] # remove item from the list
-print(test)
+# #Simple way to write
+# stu = {}
+# stu["FirstName"] = "John"
+# stu["LastName"] = "Doe"
+# stu["Grade"] = 9
+# stu["Class"] = "Algebra"
+# print(stu)
+
+
+# #Control Statement
+# name = input("Enter your name: ")
+# if name == "Jose":
+#     print(f"Hello, {name}! How is your day going so far?")
+# else:
+#     print(f'Have a great day {name}!')
+#
+
+# #For Loops
+# for i in range(5+1):
+#     if i % 2 == 0:
+#         print(i, "even")
+#     else:
+#         print(i, "odd")
+
+sum1 = 0
+for j in range(5+1):
+    sum1 = (sum1 + j)
+    print(j, sum1)
