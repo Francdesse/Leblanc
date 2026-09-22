e5e157c3e7d82a3d015610e580bb43429f0b45a0
2 Trois <2trois@2s-MacBook-Air.local>
2026-09-22 19:10:28 -0400

    Python Basics Day2

--- Learning_Python_Basics/Day1.py
+++ Learning_Python_Basics/Day1.py
@@ -46,7 +46,3 @@
 #     else:
 #         print(i, "odd")
 
-sum1 = 0
-for j in range(5+1):
-    sum1 = (sum1 + j)
-    print(j, sum1)
---
+++ Learning_Python_Basics/Day2.py
@@ -0,0 +1,20 @@
+# #the sum of numbers
+# sum1 = 0
+# for j in range(5+1):
+#     sum1 = (sum1 + j)
+#     print(j, sum1)
+
+#printing numbers from numbers
+# sum = 0
+# for i in range(1,10,2):
+#     sum = sum + i
+#     print(i, sum)
+#
+# print('The sum is equal to', sum)
+
+
+#Working with functions
+def test(joke):
+    return 'This is a test '+joke
+
+print(test("Yo Mama"))
